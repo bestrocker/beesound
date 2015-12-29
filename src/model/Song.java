a251ffa10eb91038810a5f24ef6d0f44ff24e21c
Tiziano De Cristofaro <tiziano.decristofaro@studio.unibo.it>
2015-12-29 13:15:54 +0100

    new classes Manager and Song created and added to tracker.

---
+++ src/model/Song.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.nio.file.Path;
+
+public class Song {
+    
+    private static String DEFAULT = "Unknown";
+    
+    private String title;
+    private String album;
+    private String artist;
+    private String genre;
+    private double duration;
+    private Path path;
+
+    public Song() {
+        this.title = DEFAULT;
+        this.album = DEFAULT;
+        this.artist = DEFAULT;
+        this.genre = DEFAULT;
+    }    
+    
+    public Song(String title, String album, String artist, String genre, double duration, Path path) {
+        super();
+        this.title = title;
+        this.album = album;
+        this.artist = artist;
+        this.genre = genre;
+        this.duration = duration;
+        this.path = path;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAlbum() {
+        return this.album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getArtist() {
+        return this.artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getDuration() {
+        return this.duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public Path getPath() {
+        return this.path;
+    }
+
+    public void setPath(Path path) {
+        this.path = path;
+    }
+}
