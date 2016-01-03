2549b5b31f61abfedcf09b7388bd82ee1ace366c
Scola Carlo Alberto <carloalberto.scola@studio.unibo.it>
2016-01-03 17:21:27 +0100

    Created AudioController class and tested it with playlist created shuffle and linear reproducing "strategy"

--- src/Controller/TagInfo.java
+++ src/Controller/TagInfo.java
@@ -27,7 +27,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.Vector;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
--- src/Controller/test.java
+++ src/Controller/test.java
@@ -3,21 +3,30 @@ package Controller;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         
         String dir = "/home/bestrocker221/Dropbox/Musica/";
         File mydir = new File(dir);
-        System.out.println(mydir.isDirectory());
-        System.out.println("list()"+ Arrays.deepToString(mydir.list()) + 
-                "\nlistFiles"+Arrays.deepToString(mydir.listFiles()));
-        File[] myfiles = new File[(int) mydir.length()];
+        
         String song = "James Bay - Hold Back The River.mp3";
         
+        List<String> list = new ArrayList<>();
+        Arrays.asList(mydir.listFiles()).stream()
+                                   .filter(i->i.getAbsolutePath().endsWith("mp3"))
+                                   .forEach(i->list.add(i.getAbsolutePath()));
+        list.forEach(System.out::println);
+        AudioController audio = new AudioController();
+        audio.setPlaylist(list);
+        audio.play();
+        Thread.sleep(500);
+        System.out.println("seeking");
         
+        //audio.seek(1000);
     }
 
 }
