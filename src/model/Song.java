dc7e72e3da026ecbdd4a58413d1a59ca1538d1ac
Tiziano De Cristofaro <tiziano.decristofaro@studio.unibo.it>
2016-01-04 16:55:52 +0100

    in class Song added new fields year, bitRate, reproductionsCounter and size. Getters and setters have been added and constructor has been updated.

--- src/model/Song.java
+++ src/model/Song.java
@@ -3,29 +3,35 @@ package model;
 import java.nio.file.Path;
 
 public class Song {
-    
-    private static String DEFAULT = "Unknown";
-    
+       
     private String title;
     private String album;
     private String artist;
     private String genre;
-    private double duration;
+    private String year;    
+    private double duration;    
+    private int bitRate;
+    private int reproductionsCounter;
+    private long size;
     private Path path;
 
-    public Song() {
+    /*public Song() {
         this.title = DEFAULT;
         this.album = DEFAULT;
         this.artist = DEFAULT;
         this.genre = DEFAULT;
-    }    
+    }*/    
     
-    public Song(String title, String album, String artist, String genre, double duration, Path path) {
+    public Song(String title, String album, String artist, String genre, String year,
+            double duration, int bitRate, long size, Path path) {
         this.title = title;
         this.album = album;
         this.artist = artist;
         this.genre = genre;
         this.duration = duration;
+        this.bitRate = bitRate;
+        this.reproductionsCounter = 0;
+        this.size = size;
         this.path = path;
     }
 
@@ -75,5 +81,37 @@ public class Song {
 
     public void setPath(Path path) {
         this.path = path;
+    }
+
+    public String getYear() {
+        return this.year;
+    }
+    
+    public void setYear(String year) {
+        this.year = year;
+    }
+    
+    public int getBitRate() {
+        return this.bitRate;
+    }
+    
+    public void setBitRate(int bitRate) {
+        this.bitRate = bitRate; 
+    }
+    
+    public int getReproductionsCounter() {
+        return this.reproductionsCounter;
+    }
+    
+    public void incrementCounter() {
+        this.reproductionsCounter++;
+    }
+    
+    public long getSize() {
+        return this.size;
+    }
+    
+    public void setSize(long size) {
+        this.size = size;
     }
 }
