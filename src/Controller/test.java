aafca75335aedbd7db799c416081b49319b47c6c
Scola Carlo Alberto <carloalberto.scola@studio.unibo.it>
2016-01-05 12:25:41 +0100

    upgrading FileController

--- src/Controller/test.java
+++ src/Controller/test.java
@@ -2,13 +2,16 @@ package Controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
         String dir = "/home/bestrocker221/Dropbox/Musica/";
         File mydir = new File(dir);
@@ -21,12 +24,29 @@ public class test {
         AudioController audio = new AudioController();
         audio.setPlaylist(list);
         audio.play();
-        */
+        
         FileController fc = new FileController();
         try {
             fc.importToLibrary("/home/bestrocker221/Salmo-1984.mp3", "/home/bestrocker221/Arduino/");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        */
+        final String libraryPath = System.getProperty("user.home")+"/beesound/";
+        
+        System.out.println(System.getProperty("user.home")+"/");
+        if (Files.notExists(Paths.get(libraryPath), LinkOption.NOFOLLOW_LINKS)){
+            new File(libraryPath).mkdirs();
+            System.out.println("Library directory created");
+        }
+        if(Files.notExists(Paths.get(libraryPath+"properties.txt"), LinkOption.NOFOLLOW_LINKS)){
+            try {
+                new File(libraryPath+"properties.txt").createNewFile();
+                System.out.println("properties file created");
+            } catch (IOException e) {
+                System.err.println("cannot create properties File\n");
+                e.printStackTrace();
+            }
         }
     }
 
