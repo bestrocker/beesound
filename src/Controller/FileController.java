097f9be8641ca6d39f7e65239af3c93c49b102e9
Scola Carlo Alberto <carloalberto.scola@studio.unibo.it>
2016-01-05 12:25:41 +0100

    upgrading FileController

--- src/Controller/FileController.java
+++ src/Controller/FileController.java
@@ -1,8 +1,13 @@
 package Controller;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
@@ -11,8 +16,34 @@ import java.util.List;
 
 public  class FileController implements SystemManager{
 
+    public static final String libraryPath = System.getProperty("user.dir")+"/beesound/";
+    private BufferedReader propertiesRead;
+    private BufferedWriter propertiesWrite;
+    public File properties;
+    
     public FileController() {
         
+        if (Files.notExists(Paths.get(libraryPath), LinkOption.NOFOLLOW_LINKS)){
+            new File(libraryPath).mkdirs();
+            System.out.println("Library directory created");
+        }
+        this.properties = new File(libraryPath+"properties.txt");
+        if (Files.notExists(this.properties.toPath(), LinkOption.NOFOLLOW_LINKS)){
+            try {
+                this.properties.createNewFile();
+                System.out.println("properties file created");
+            } catch (IOException e) {
+                System.err.println("cannot create properties File\n");
+                e.printStackTrace();
+            }
+        }
+        try {
+            this.propertiesRead = new BufferedReader(new FileReader(this.properties));
+            this.propertiesWrite = new BufferedWriter(new FileWriter(this.properties));
+        } catch (IOException e) {
+            System.out.println("Buffer setted");
+            e.printStackTrace();
+        }
     }
     
     public List<File> listAllFilesInDir(final File directory) {
