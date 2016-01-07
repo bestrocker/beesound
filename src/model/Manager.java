597916d257c194b254c34327a5821e4469dbe4c7
Tiziano De Cristofaro <tiziano.decristofaro@studio.unibo.it>
2016-01-08 00:01:22 +0100

    Added Javadoc comments.

--- src/model/Manager.java
+++ src/model/Manager.java
@@ -2,22 +2,22 @@ package model;
 
 import java.nio.file.Path;
 
-/** Interface for classes that have to manage and organize the music library.
- * 
+/** 
+ * Interface for classes that have to manage and organize the music library.
  * @author tiziano
  *
  */
 public interface Manager {
     
     /**
-     * Method for adding a new song to the library list.
-     * @param song
+     * Adds a new song to the library list.
+     * @param song - the song to add to the list.
      */
     void addSongToLibrary(Song song);
     
     /**
-     * Method for serializing info of a specified song in the file system.
-     * @param path
+     * Serializes info of a specified song in the file system.
+     * @param path - the path in which data is serialized.
      */
     void serializeData(Path path);
 }
