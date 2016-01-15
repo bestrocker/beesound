50c86f8e6994a0aee6c8e0b7dedc098faacc08fc
Gianluca Cincinelli <gianluca.cincinelli@studio.unibo.it>
2016-01-15 18:29:50 +0100

    added method to ViewInterface

--- src/view/ViewInterface.java
+++ src/view/ViewInterface.java
@@ -3,6 +3,16 @@ package view;
 import Controller.ViewObserver;
 
 public interface ViewInterface {
-
+	
+	/**
+	 * Sets observer for the GUI
+	 * @param observer
+	 */
     void setObserver(final ViewObserver observer);
+    
+    /**
+     * Returns the index of the currently selected item
+     * @return
+     */
+    int getSelectedIndex();
 }
