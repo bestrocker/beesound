f062815fa9520175ffdb7bb5e04ae1cddfdbc1a2
Tiziano De Cristofaro <tiziano.decristofaro@studio.unibo.it>
2016-01-22 19:41:16 +0100

    in class LibraryManager added getCurrentSongInfo method, updated Manager interface.

---
+++ src/Controller/Controller.java
@@ -0,0 +1,312 @@
+package Controller;
+
+import java.io.File;
+import static Controller.Audio.AudioController.REPRODUCTION_STRATEGY.*;
+import java.io.IOException;
+import java.util.List;
+import Controller.Audio.AudioController;
+import Controller.Audio.MpegInfo;
+import Controller.Files.FileController;
+import static Controller.Files.FileController.*;
+import Controller.Files.Log;
+import model.LibraryManager;
+import model.Manager;
+import view.GUI;
+
+public class Controller implements ViewObserver {
+
+    private final Manager model;
+    private final GUI view;
+    private final AudioController audiocontrol;
+    private final FileController filecontrol;
+    
+    public Controller() {
+        this.model = LibraryManager.getInstance();        
+        this.filecontrol = new FileController();
+        this.audiocontrol = new AudioController(model);
+        loadInfoToLibrary();
+        this.view = new GUI();
+        this.view.setObserver(this);
+       
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void newPlaylistFile(final String name){
+        final String temp = playlistDirPath+name+".txt";
+        if(this.filecontrol.notExist(temp)){
+            this.filecontrol.createNewFile(name,playlistDirPath);
+            this.model.newPlaylist(name,temp);
+        }
+        //AGGIUNGERE CODICE "ESISTE GIà"
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addSongInPlaylist(final String song, final String playlist){
+       this.model.addSongInPlaylist(song,playlist);
+        /* playlist.addSong(song);*/
+       final String songPath= this.model.getSongPath(song);
+       final String playlistPath = this.model.getPlaylistPath(playlist);
+        try {
+            if(!this.filecontrol.getPlaylistSongs(new File(playlistPath)).contains(songPath)){
+                this.filecontrol.appendToFile(songPath,playlistPath);
+                Log.INFO(songPath+ " Added to playlist "+playlist);
+            }
+        } catch (IOException e) {
+            Log.ERROR("Can't addSongInPlaylist");
+            e.printStackTrace();
+        }
+    }
+    
+    private void loadInfoToLibrary() {
+        final MpegInfo info = MpegInfo.getInstance();
+        this.filecontrol.listAllSongPath()
+                        .stream()
+                        .forEach(i->{
+                             if(info.load(new File(i))){
+                                 this.addSong(i, info);
+                             }
+                         });
+        Log.INFO("Mpeg tag succesfully loaded into the library.");
+    }
+    
+    public static void main(String[] args) throws InterruptedException{
+        Controller c = new Controller();
+        
+       // List<String> l = c.filecontrol.listAllSongPath();
+        
+    //    List<Song> lsong = c.model.getSongList();
+        
+       // c.audiocontrol.addSongInPlaylist("/home/bestrocker221/Dropbox/Musica/holdbacktheriver.mp3");
+      //  c.audiocontrol.setReproductionStrategy(REPRODUCTION_STRATEGY.SHUFFLE);
+    //    c.audiocontrol.setPlaylist(c.model.getPlaylistList().get(0));
+       /* 
+        c.newPlaylistFile("bella");
+        System.out.println("CREATED : " +c.model.getPlaylistList().get(0).getPath());
+        
+       c.addSongInPlaylist(c.model.getSongList().get(0), c.model.getPlaylistList().get(0));
+       
+        System.out.println("TRYING TO SET PLAYLIST "+c.model.getPlaylistList().get(0).getPath());*/
+        //c.audiocontrol.setPlaylist(c.model.getPlaylistList().get(0));
+       
+        //c.audiocontrol.playPlayer();
+       // Thread.sleep(10000);
+      //  c.playButton(c.model.getSongList().get(3));
+      //  System.out.println(c.model.getSongList().get(0).getTitle() + " added to "+ c.model.getPlaylistList().get(0).getName() );
+       // System.out.println("import fatto");
+       /*
+        while (true){
+        Thread.sleep(2000);
+        c.audiocontrol.nextPlayer();
+        for(Song i : c.model.getSongList()){
+            System.out.println("INIZIO");
+            System.out.println(i.getPath());
+            System.out.println("title "+i.getTitle());
+            System.out.println("album" + i.getAlbum());
+            System.out.println("artist "+i.getArtist());
+            System.out.println("bitrate "+i.getBitRate());
+            System.out.println("genre "+i.getGenre());
+            System.out.println("size "+i.getSize());
+            System.out.println("duration "+i.getDuration().getMin()+":"+ i.getDuration().getSec());
+            System.out.println("counter " + i.getReproductionsCounter());
+            System.out.println("FINE");
+        }
+        }*/
+    }
+    
+    private void addSong(final String songPath, final MpegInfo info){
+        final String alternativeTitle = songPath.substring(songPath.lastIndexOf(sep)+1,songPath.length()-4);
+        if(!this.model.getSongTitles().contains(alternativeTitle)){
+            this.model.addSongToLibrary(
+                     (info.getTitle().orElse(alternativeTitle)) ,(info.getAlbum().orElse("")) 
+                     ,(info.getArtist().orElse("")) ,(info.getGenre().orElse("")) ,(info.getDurationInMinutes())
+                    ,(info.getBitRate()) ,(info.getSize()) ,(songPath) ,(info.getCopyright()) ,(info.getChannels())
+                    ,(info.getVersion()),(info.getSamplingRate()),(info.getChannelsMode())
+                    );
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addSong(final String songPath) {
+      //  String path =this.filecontrol.importToLibrary(songPath);
+        MpegInfo info = MpegInfo.getInstance();
+        info.load(new File(this.filecontrol.importToLibrary(songPath)));
+        this.addSong(songPath, info);
+        
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> showAllSong() {
+        return this.model.getSongTitles();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> showAllPlaylist() {
+        return this.model.getPlaylistNames();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void playButton() {
+        this.audiocontrol.playPlayer();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void pauseButton() {
+        this.audiocontrol.togglePause();
+        
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void stopButton() {
+       this.audiocontrol.stopPlayer();
+        
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setVolumeButton(final double volume) {
+        this.audiocontrol.setVolume(volume);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void skipTo(final long toBytes) {
+        this.audiocontrol.seekPlayer(toBytes);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addSongInReproductionPlaylist(final String song,final REPRODUCE when) {
+        this.model.addSongInPlaylist(song, when.getVal());
+        if(when.getVal()){
+            this.audiocontrol.setReproduceNowBoolean(true);
+            this.audiocontrol.playPlayer();
+        }
+    }
+    
+    public enum REPRODUCE{
+        NOW(true),AFTER(false);
+        
+        final private boolean val;
+        private REPRODUCE(final boolean b) {
+           this.val = b;
+        }
+        public boolean getVal(){
+            return this.val;
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setShuffleMode() {
+        this.audiocontrol.setReproductionStrategy(SHUFFLE);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void linearMode() {
+        this.audiocontrol.setReproductionStrategy(LINEAR);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> showAllAlbum() {
+        return this.model.getAlbumTitles();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> showAllGenre() {
+       return this.model.getGenreNames();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> showAllArtist() {
+        return this.model.getArtistNames();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void nextTrack() {
+        this.audiocontrol.nextPlayer();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void prevTrack() {
+        this.audiocontrol.prevPlayer();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> showReproductionPlaylist() {
+        return this.model.getInReproductionTitles();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void removeSong(final String songTitle) {
+        this.model.removeSong(songTitle);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void removeSongFromQueue(final String songTitle) {
+        this.model.removeSongFromQueue(songTitle);
+    }
+
+    @Override
+    public List<String> showPlaylistSong(String playlistName) {
+        this.model.showPlaylistSong(playlistName);
+    }
+}
---
+++ src/Controller/Files/FileController.java
@@ -0,0 +1,137 @@
+package Controller.Files;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public  class FileController implements SystemManager{
+    public static final String sep = System.getProperty("file.separator");
+    public static final String libraryPath = System.getProperty("user.home")+sep+"beesound"+sep;
+    public static final  String logPath = libraryPath+"LOG.txt";
+    public static final String propPath = libraryPath+"properties.txt";
+    public static final String musicDirPath = libraryPath + "Music"+sep;
+    public static final String playlistDirPath = libraryPath + "Playlist"+sep;
+    private  PrintWriter writer;
+    
+    /**
+     * Initialize FileController, Libraries folder, Logger, Properties.
+     */
+    public FileController() {
+        initialize();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean notExist(final String srcPath){
+        return (Files.notExists(Paths.get(srcPath), LinkOption.NOFOLLOW_LINKS));
+    }
+    
+    private void initialize() {
+        if (notExist(libraryPath)){
+            new File(libraryPath).mkdirs();
+        }
+        Log.initializeLogger();
+        Log.PROGRAM("Root Library folder loaded. STARTING PROGRAM");
+        if (notExist(musicDirPath)){
+            new File(musicDirPath).mkdirs();
+            Log.PROGRAM("Music Directory created");
+        }
+        if (notExist(playlistDirPath)){
+            new File(playlistDirPath).mkdirs();
+            Log.PROGRAM("Playlist Directory created");
+        }
+        if (notExist(propPath)){
+            createNewFile("" ,propPath);
+        }
+        /*
+        try {
+            //this.propertiesRead = new BufferedReader(new FileReader(this.properties));
+            
+        } catch (IOException e) {
+            Log.ERROR("Buffer and logger not set, error in FileController constructor");
+            e.printStackTrace();
+        }*/
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> listAllSongPath(final File directory) {        
+        return Arrays.asList(directory.listFiles()).stream()
+                .filter(i->i.getAbsolutePath().endsWith("mp3"))
+                .map(i->i.getAbsolutePath())
+                .collect(Collectors.toList());
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> listAllSongPath(){
+        return listAllSongPath(new File(musicDirPath));
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> getPlaylistSongs(final File playlist) throws IOException{
+        return Files.readAllLines(playlist.toPath());
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public  String importToLibrary(final String pathSource) {
+       final String d = musicDirPath + pathSource.substring(pathSource.lastIndexOf(sep));
+       if(Files.notExists(Paths.get(d), LinkOption.NOFOLLOW_LINKS)){
+           try{
+               Files.copy(Paths.get(pathSource),
+                   Paths.get(d),
+                   StandardCopyOption.COPY_ATTRIBUTES);
+               Log.INFO(pathSource +" added to library");
+               return d;
+           } catch (Exception e){
+               e.printStackTrace();
+               Log.ERROR("Can't importToLibrary, error during copy");
+           }
+       }
+       return d;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @throws IOException 
+     */
+    public void createNewFile(final String name,final String dstPath) {
+        try {
+            new File(dstPath+name+".txt").createNewFile();
+        } catch (IOException e) {
+            Log.ERROR("Can't createNewFile()");
+            e.printStackTrace();
+            return;
+        }
+        Log.PROGRAM("New file "+name+" created : "+name+dstPath);
+    }
+    
+    public void appendToFile(final String msg, final String path){
+        try {
+            this.writer = new PrintWriter(new FileWriter(new File(path),true),true);
+            this.writer.println(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
