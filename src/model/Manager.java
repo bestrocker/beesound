2d32b1e1d7aaf4301c5ad02c5f50800598b757a0
Tiziano De Cristofaro <tiziano.decristofaro@studio.unibo.it>
2016-01-25 11:58:43 +0100

    in class LibraryManager modified getLibraryInfo method return type from Map<String, Integer> to List<Integer>. Update Manager interface.

--- src/model/Manager.java
+++ src/model/Manager.java
@@ -141,7 +141,7 @@ public interface Manager {
      * Returns a map containing info about total number of songs and total audio time in library.
      * @return a map containing info about total number of songs and total audio time in library.
      */
-    Map<String, Integer> getLibraryInfo();
+    List<Integer> getLibraryInfo();
     
     /**
      * Returns a list containing the most listened song titles.
